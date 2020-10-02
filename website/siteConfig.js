9349def8c7302aafc7dff7f2ccc21387776b36be
Marianna Ghirardelli <43092418+maghirardelli@users.noreply.github.com>
2020-10-02 15:24:47 -0400

    Started tech portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,11 +11,12 @@
 // List of projects/orgs using your project for the users page.
 const users = [];
 
+// How you would like to make the site look:
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Marianna Ghirardelli', // Title for your website.
+    tagline: 'Math && CS && Food && Books && Photography',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://maghirardelli.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +24,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'maghirardelli',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +68,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
-        twitter: '',
-        github: '',
+        linkedin: 'marianna-ghirardelli',
+        twitter: 'marvelouscode',
+        github: 'maghirardelli',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
